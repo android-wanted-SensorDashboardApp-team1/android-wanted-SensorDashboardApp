7f7eba3706c1c52976bbc8e9c52d52aa21fae69a
gksgpd97 <gksgpd97@naver.com>
2022-09-30 11:07:26 +0900

    feat: swipe 1차 구현 #17

--- app/src/main/java/com/preonboarding/sensordashboard/presentation/MainActivity.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.preonboarding.sensordashboard.presentation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.preonboarding.sensordashboard.R
 import com.preonboarding.sensordashboard.databinding.ActivityMainBinding
+import com.preonboarding.sensordashboard.domain.model.SensorData
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -30,12 +35,42 @@ class MainActivity : AppCompatActivity() {
             recyclerViewAdapter = MainRecyclerViewAdapter(sensorViewModel)
             adapter = recyclerViewAdapter
         }
+
+        val swipeController = SwipeController(recyclerViewAdapter)
+        ItemTouchHelper(swipeController).attachToRecyclerView(binding.recyclerviewMain)
+
+        recyclerViewAdapter.setOnSwipeClickListener(object :
+            MainRecyclerViewAdapter.OnSwipeClickListener {
+            override fun onRightClick(sensorData: SensorData) {
+                //다시보기 페이지로 이동시켜야함. id 넘겨줘야할듯. 물어보자
+            }
+
+            override fun onLeftClick(sensorData: SensorData) {
+                sensorViewModel.deleteSensorData(sensorData.id)
+            }
+        })
+
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 sensorViewModel.sensorsFlow.collectLatest { sensors ->
                     (binding.recyclerviewMain.adapter as MainRecyclerViewAdapter).submitList(sensors)
                 }
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item?.itemId) {
+            R.id.item_main_measure -> {
+                //화면전환 코드 추가 필요
+                super.onOptionsItemSelected(item)
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/preonboarding/sensordashboard/presentation/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/preonboarding/sensordashboard/presentation/MainRecyclerViewAdapter.kt
@@ -9,7 +9,10 @@ import com.preonboarding.sensordashboard.databinding.ItemMainRecyclerviewBinding
 import com.preonboarding.sensordashboard.domain.model.SensorData
 
 class MainRecyclerViewAdapter(val viewModel: SensorViewModel) :
-    ListAdapter<SensorData, MainRecyclerViewAdapter.ViewHolder>(SensorDiffCallback()) {
+    ListAdapter<SensorData, MainRecyclerViewAdapter.ViewHolder>(SensorDiffCallback()),
+    ItemTouchHelperListener {
+
+    private lateinit var onSwipeClickListener: OnSwipeClickListener
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
             ItemMainRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -29,7 +32,33 @@ class MainRecyclerViewAdapter(val viewModel: SensorViewModel) :
             binding.executePendingBindings()
         }
     }
+
+    fun setOnSwipeClickListener(listener: OnSwipeClickListener) {
+        onSwipeClickListener = listener
+    }
+
+    override fun onItemMove(from_position: Int, to_position: Int): Boolean {
+        return false
+    }
+
+    override fun onItemSwipe(position: Int) {
+
+    }
+
+    override fun onLeftClick(position: Int, viewHolder: RecyclerView.ViewHolder?) {
+        onSwipeClickListener.onLeftClick(getItem(position))
+    }
+
+    override fun onRightClick(position: Int, viewHolder: RecyclerView.ViewHolder?) {
+        onSwipeClickListener.onRightClick(getItem(position))
+    }
+
+    interface OnSwipeClickListener {
+        fun onLeftClick(sensorData: SensorData)
+        fun onRightClick(sensorData: SensorData)
+    }
 }
+
 
 class SensorDiffCallback : DiffUtil.ItemCallback<SensorData>() {
     override fun areItemsTheSame(oldItem: SensorData, newItem: SensorData): Boolean {
